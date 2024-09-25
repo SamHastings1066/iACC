89e8083c3da5d4403efecd8dfd31039911a5bf68
Sam Hastings <samhastingsis@gmail.com>
2024-09-25 18:24:42 -0400

    Remove untyped initialiser for ItemViewModel.

--- iACC/ListViewController.swift
+++ iACC/ListViewController.swift
@@ -185,31 +185,25 @@ struct ItemViewModel {
     let title: String
     let subTitle: String
     let select: () -> Void
-    
-    init(_ item: Any, longDateStyle: Bool, selection: @escaping  () -> Void) {
-        if let friend = item as? Friend {
-            self.init(friend: friend, selection: selection)
-        } else if let card = item as? Card {
-            self.init(card: card, selection: selection)
-        } else if let transfer = item as? Transfer {
-            self.init(transfer: transfer, longDateStyle: longDateStyle, selection: selection)
-        } else {
-            fatalError("unknown item: \(item)")
-        }
-    }
-    
+}
+
+extension ItemViewModel {
     init(friend: Friend, selection: @escaping () -> Void) {
         title = friend.name
         subTitle = friend.phone
         select = selection
     }
-    
+}
+
+extension ItemViewModel {
     init(card: Card, selection: @escaping () -> Void) {
         title = card.number
         subTitle = card.holder
         select = selection
     }
-    
+}
+
+extension ItemViewModel {
     init(transfer: Transfer, longDateStyle: Bool, selection: @escaping () -> Void) {
         let numberFormatter = Formatters.number
         numberFormatter.numberStyle = .currency
